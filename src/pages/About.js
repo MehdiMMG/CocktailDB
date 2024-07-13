10e342895cb3a11c94dd5a86142ed7d90c247ac3
MMGMehdi <mehdimonjezimg@gmail.com>
2024-07-13 23:16:30 +0330

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing
+         elit. Adipisci esse alias, blanditiis voluptas rem 
+         hic autem sequi similique sapiente perspiciatis 
+         veritatis quam, doloribus praesentium sed debitis 
+         possimus voluptatem, obcaecati officiis ab aut 
+         illum explicabo accusamus molestias? Inventore 
+         iure ipsam hic.
+      </p>
+    </section>
+  )
+}
+
+export default About
